29dce6ca248916dd60764dde39f16f2476a7c271
Mateusz Furga <matfurga@gmail.com>
2021-11-05 19:23:51 +0100

    Update lab2

--- lab2/zad12/script.js
+++ lab2/zad12/script.js
@@ -2,9 +2,14 @@ const colors = document.querySelectorAll('.color');
 colors.forEach(c => c.addEventListener('click', color));
 function color() {
     let color =  window.getComputedStyle(document.getElementById(this.id)).backgroundColor;
-    let back = document.getElementById("bgcolor");
-    back.style.backgroundImage = 'linear-gradient(45deg,' + color+ ', ' + color +')';
+    document.documentElement.style.setProperty("--current", color);
     colors.forEach(c => c.classList.remove('active'));
-    colors.forEach(c => c.classList.remove('active'));
+    this.classList.add('active');
+}
+
+const sizes = document.querySelectorAll('.size');
+sizes.forEach(c => c.addEventListener('click', size));
+function size() {
+    sizes.forEach(c => c.classList.remove('active'));
     this.classList.add('active');
 }
--- lab2/zad9/sections/script.js
+++ lab2/zad9/sections/script.js
@@ -1,31 +1,29 @@
-$(function() {
-    $(document).scroll(function() {
-        var nav = $(".header");
+$(document).scroll(function() {
+    var nav = $(".header");
 
-        var curr = $(this).scrollTop();
+    var curr = $(this).scrollTop();
 
-        var sec1 = $("#sec1").offset().top;
-        var sec2 = $("#sec2").offset().top;
-        var sec3 = $("#sec3").offset().top;
-        var sec4 = $("#sec4").offset().top;
+    var sec1 = $("#sec1").offset().top;
+    var sec2 = $("#sec2").offset().top;
+    var sec3 = $("#sec3").offset().top;
+    var sec4 = $("#sec4").offset().top;
 
-        if (curr > sec1) {
-            $(".navbar").css("background", "#ffc6c6");
-            $(".navbar button").css("color", "#0006fb");
-        } else {
-            $(".navbar").css("background", "#fff");
-            $(".navbar button").css("color", "#fff");
-        }
+    if (curr > sec1) {
+        $(".navbar").css("background", "#ffc6c6");
+        $(".navbar button").css("color", "#0006fb");
+    } else {
+        $(".navbar").css("background", "#fff");
+        $(".navbar button").css("color", "#fff");
+    }
 
-        if (curr > sec2) {
-            $(".navbar").css("background", "#ff8383");
-            $(".navbar button").css("color", "#00fb71");
-        }
+    if (curr > sec2) {
+        $(".navbar").css("background", "#ff8383");
+        $(".navbar button").css("color", "#00fb71");
+    }
 
-        if (curr > sec3) {
-            $(".navbar").css("background", "#ff4c4c");
-            $(".navbar button").css("color", "#fbe900");
-        }
+    if (curr > sec3) {
+        $(".navbar").css("background", "#ff4c4c");
+        $(".navbar button").css("color", "#fbe900");
+    }
 
-    });
 });
