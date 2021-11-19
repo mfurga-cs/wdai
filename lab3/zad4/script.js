f8b13c1c3c5737e03a0d419c733de86fff44303b
Mateusz Furga <matfurga@gmail.com>
2021-11-19 21:05:42 +0100

    Update zad4

--- lab3/zad4/script.js
+++ lab3/zad4/script.js
@@ -1,21 +1,31 @@
 "use strict";
 
+const counter = document.getElementById("counter");
+const increment = document.getElementById("increment");
+const handle = document.getElementById("handle");
+const reset = document.getElementById("reset");
+
 let isOn = false;
 let count = 0;
 
 function updateCounter(value) {
   count = value;
-  document.getElementById("counter").innerText = count;
+  counter.innerText = value;
 }
 
-document.getElementById("increment").addEventListener("click", function (event) {
+function incrementEvent() {
+  updateCounter(count + 1);
+}
+
+handle.addEventListener("click", () => {
+  isOn = !isOn;
+
   if (isOn) {
-    updateCounter(count + 1);
+    increment.addEventListener("click", incrementEvent);
+  } else {
+    increment.removeEventListener("click", incrementEvent);
+    updateCounter(0);
   }
 });
 
-document.getElementById("handle").addEventListener("click", function (event) {
-  updateCounter(0);
-  isOn = !isOn;
-});
-
+reset.addEventListener("click", () => { updateCounter(0) });
